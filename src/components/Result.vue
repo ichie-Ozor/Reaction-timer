2dc504f9d7889f94456a5a9d0f80965cec860534
Ichie Ozor <simeon_mc2000@yahoo.com>
2023-11-14 18:42:40 +0100

    first commit

---
+++ src/components/Result.vue
@@ -0,0 +1,34 @@
+<template>
+   <div @end="endPlay">
+       <p>Reaction time - {{ show }} ms</p>
+       <p class="rank">{{ rank }}</p>
+   </div>
+</template>
+
+<script>
+export default {
+    name: "Result",
+    props: ['show'],
+    data(){
+        return {
+            rank: null
+        }
+    },
+    mounted(){
+        if(this.show < 250) {
+            this.rank = 'Ninja finger'
+        } else if (this.show < 400) {
+            this.rank = 'Rapid Reflexes'
+        } else  {
+            this.rank = 'Snail pace...'
+        }
+    }
+}
+</script>
+<style>
+.rank{ 
+    font-size: 1.4rem;
+    color: #0faf87;
+    font-weight: bold;
+    }
+</style>
